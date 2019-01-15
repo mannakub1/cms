db5f0921cbcf5991e723dfff72eae2c3ff25bbbd
mannakub1 <pholberdee_n@silpakorn.edu>
2019-01-15 11:59:49 +0700

    clear file api

--- app/api/cms/v1/admin_api/get.rb
+++ app/api/cms/v1/admin_api/get.rb
@@ -4,21 +4,7 @@ module Cms::V1::AdminAPI
 
         resource '/admin/get' do
 
-            get '/test_api' do 
-                {"content": "hello world!!"}
-            end
-
-            get '/test_api_2' do 
-                {"content": "test api"}
-            end
-
-            get '/test_api_3' do 
-                {"content": "test api 3"}
-            end
-
-            get '/test_api_4' do 
-                {"content": "test api 3"}
-            end
+          
         end
     end
 end
