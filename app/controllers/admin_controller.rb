545b30eb403482e87b9a08f55914704e6634c795
mannakub1 <pholberdee_n@silpakorn.edu>
2019-01-18 17:28:16 +0700

    add: before action check_token to controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,9 @@
 class AdminController < ApplicationController
 
+    before_action :find_token, :only => [:page]
+    
+    attr_reader :token
+
     def home
         @header = "Home"
         @contents = "Welcome setting Page"
@@ -9,5 +13,15 @@ class AdminController < ApplicationController
     def page
         @header = "Page"
         render layout: "layout_admin"
+    end
+ 
+    private
+
+    def find_token
+        @token = ApiKey.last.access_token
+
+        if !Admin::AdminAction.new.check_token(@token) 
+            redirect_to admin_home_path
+        end
     end
 end
